c0ce9566734302d6580b4e5b1e2ee1363c19fda9
agglnxz <140498082+agglnxz@users.noreply.github.com>
2025-06-05 10:46:35 +0700

    first commit

---
+++ bin/project_uas.dart
@@ -0,0 +1,5 @@
+import 'package:project_uas/project_uas.dart' as project_uas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_uas.calculate()}!');
+}
---
+++ test/project_uas_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_uas/project_uas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
