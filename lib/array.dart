dcb6cb308eea40f140837f1d49ee1fcdf95c018f
Baktiarr <124052346+Baktiar-Arch@users.noreply.github.com>
2025-06-11 10:39:24 +0700

    Menambhkan array

---
+++ lib/array.dart
@@ -0,0 +1,20 @@
+final List<Map<String, dynamic>> jadwalLatihanVoli = [
+    {
+        'hari': 'Senin',
+        'waktu': '16:00 - 18:00',
+        'tempat': 'Gor Kampus',
+        'pelatih': 'Coach Andi',
+    },
+    {
+        'hari': 'Rabu',
+        'waktu': '16:00 - 18:00',
+        'tempat': 'Gor Kampus',
+        'pelatih': 'Coach Budi',
+    },
+    {
+        'hari': 'Jumat',
+        'waktu': '16:00 - 18:00',
+        'tempat': 'Gor Kampus',
+        'pelatih': 'Coach Andi',
+    },
+];
